3b1c8e7769c6b3146859175cbb1fe820b68bfc62
Rune Loyning <rune@loyning.net>
2015-08-11 14:57:55 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
+#
+# Copyright 2015 Rune Loyning
+#
+# http://github.com/loyning/python-24so
+#
+
+import os
+import re
+
+from setuptools import find_packages
 from distutils.core import setup
+
+with open(os.path.join('intercom', '__init__.py')) as init:
+    source = init.read()
+    m = re.search("__version__ = '(\d+\.\d+(\.(\d+|[a-z]+))?)'", source, re.M)
+    __version__ = m.groups()[0]
+
+with open('README.rst') as readme:
+    long_description = readme.read()
 
 setup(
     name="python-24so",
-    version='0.1',
-    url='http://github.com/loyning/python-24so/',
+    version=__version__,
+    description='Python wrapper for the 24sevenoffice SOAP api',
+    long_description=long_description,
     author='Rune Loyning',
     author_email='rune@loyning.net',
-    long_description=open('README.rst').read(),
-    description='Python wrapper for the 24sevenoffice SOAP api',
-    packages=['python-24so']
+    url='http://github.com/loyning/python-24so/',
+    keywords='24sevenoffice crm python',
+    classifiers=[],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["suds-fc", ],
 )
